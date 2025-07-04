5c69fcea29152610bc273f5c3fd7d11b5958838c
Diego Anguita M. <tralcan@gmail.com>
2025-07-04 01:13:21 +0000

    por favor deja el nombre de la app como Split it Right

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'MyBill',
+  title: 'Split it Right',
   description: 'Divide la cuenta del restaurante con amigos sin esfuerzo.',
 };
 
