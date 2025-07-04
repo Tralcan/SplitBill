3bc74a0dc5bca525e8e30afdb5d8ae7d225088f4
Diego Anguita M. <tralcan@gmail.com>
2025-07-04 14:18:17 +0000

    Algo ha pasado, antes todo funcionaba bien (con archivos pequeños) pero

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import next from '@genkit-ai/next';
 
 export const ai = genkit({
   plugins: [
-    next(),
     googleAI({
       apiKey: process.env.GOOGLE_API_KEY,
     }),
